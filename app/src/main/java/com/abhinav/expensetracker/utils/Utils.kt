449f4cf849b2b3289273cb1d33e9a493c7289915
Abhinav Datta <74416206+abhinavdatta@users.noreply.github.com>
2026-05-17 23:58:07 +0530

    Add files via upload

---
+++ app/src/main/java/com/abhinav/expensetracker/utils/Utils.kt
@@ -0,0 +1,74 @@
+package com.abhinav.expensetracker.utils
+
+import com.abhinav.expensetracker.R
+import com.abhinav.expensetracker.data.model.ExpenseEntity
+import java.text.NumberFormat
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+
+object Utils {
+
+    fun formatDateToHumanReadableForm(dateInMillis: Long): String {
+        val dateFormatter = SimpleDateFormat("dd/MM/YYYY", Locale.getDefault())
+        return dateFormatter.format(dateInMillis)
+    }
+
+    fun formatDateForChart(dateInMillis: Long): String {
+        val dateFormatter = SimpleDateFormat("dd-MMM", Locale.getDefault())
+        return dateFormatter.format(dateInMillis)
+    }
+
+    fun formatCurrency(amount: Double, locale: Locale = Locale("en", "in")): String {
+        val currencyFormatter = NumberFormat.getCurrencyInstance(locale)
+        return currencyFormatter.format(amount)
+    }
+
+    fun formatDayMonthYear(dateInMillis: Long): String {
+        val dateFormatter = SimpleDateFormat("MMM dd, yyyy", Locale.getDefault())
+        return dateFormatter.format(dateInMillis)
+    }
+
+    fun formatDayMonth(dateInMillis: Long): String {
+        val dateFormatter = SimpleDateFormat("dd/MMM", Locale.getDefault())
+        return dateFormatter.format(dateInMillis)
+    }
+
+    fun formatToDecimalValue(d: Double): String {
+        return String.format("%.2f", d)
+    }
+
+    fun formatStringDateToMonthDayYear(date: String): String {
+        val millis = getMillisFromDate(date)
+        return formatDayMonthYear(millis)
+    }
+
+    fun getMillisFromDate(date: String): Long {
+        return getMilliFromDate(date)
+    }
+
+    fun getMilliFromDate(dateFormat: String?): Long {
+        var date = Date()
+        val formatter = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+        try {
+            date = formatter.parse(dateFormat)
+        } catch (e: ParseException) {
+            e.printStackTrace()
+        }
+        println("Today is $date")
+        return date.time
+    }
+
+    fun getItemIcon(item: ExpenseEntity): Int {
+        return when (item.title) {
+            "Hotstar" -> R.drawable.ic_netflix // Using netflix icon as placeholder for streaming
+            "Chai Point" -> R.drawable.ic_starbucks // Using starbucks icon as placeholder for cafe
+            "PhonePe", "GPay", "Paytm" -> R.drawable.ic_upwork // Placeholder for payment apps
+            "SBI", "HDFC", "ICICI", "Axis", "PNB" -> R.drawable.ic_upwork // Placeholder for banks
+            else -> R.drawable.ic_upwork
+        }
+    }
+
+}
