6b036681dba5fd5e7b67634b9d1123c458ca42b6
Abhinav Datta <74416206+abhinavdatta@users.noreply.github.com>
2026-05-17 23:58:34 +0530

    Delete expensetracker directory

--- expensetracker/ExpenseTrackerApp.kt
+++
@@ -1,9 +0,0 @@
-package com.abhinav.expensetracker
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class ExpenseTrackerApp: Application() {
-
-}
--- expensetracker/data/model/ExpenseSummary.kt
+++
@@ -1,7 +0,0 @@
-package com.abhinav.expensetracker.data.model
-
-data class ExpenseSummary(
-    val type: String,
-    val date: String,
-    val total_amount: Double
-)
